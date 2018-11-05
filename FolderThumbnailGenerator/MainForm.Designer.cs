151dd77afc9b40ee1fcb1d315e6a5e58588b7cf6
108EAA0A <k_108e@outlook.jp>
2018-11-06 01:06:44 +0900

    初期バージョン

---
+++ FolderThumbnailGenerator/MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FolderThumbnailGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_DirectorySelect = new System.Windows.Forms.Button();
+            this.checkBox_IsRecursion = new System.Windows.Forms.CheckBox();
+            this.textBox_DirectoryName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox_IsOverwrite = new System.Windows.Forms.CheckBox();
+            this.checkBox_IsHiddenFile = new System.Windows.Forms.CheckBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button_Execution = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_DirectorySelect
+            // 
+            this.button_DirectorySelect.Location = new System.Drawing.Point(312, 34);
+            this.button_DirectorySelect.Name = "button_DirectorySelect";
+            this.button_DirectorySelect.Size = new System.Drawing.Size(75, 23);
+            this.button_DirectorySelect.TabIndex = 0;
+            this.button_DirectorySelect.Text = "選択";
+            this.button_DirectorySelect.UseVisualStyleBackColor = true;
+            this.button_DirectorySelect.Click += new System.EventHandler(this.button_DirectorySelect_Click);
+            // 
+            // checkBox_IsRecursion
+            // 
+            this.checkBox_IsRecursion.AutoSize = true;
+            this.checkBox_IsRecursion.Checked = true;
+            this.checkBox_IsRecursion.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_IsRecursion.Location = new System.Drawing.Point(64, 106);
+            this.checkBox_IsRecursion.Name = "checkBox_IsRecursion";
+            this.checkBox_IsRecursion.Size = new System.Drawing.Size(199, 16);
+            this.checkBox_IsRecursion.TabIndex = 1;
+            this.checkBox_IsRecursion.Text = "サブディレクトリまで再帰的に処理する";
+            this.checkBox_IsRecursion.UseVisualStyleBackColor = true;
+            // 
+            // textBox_DirectoryName
+            // 
+            this.textBox_DirectoryName.Location = new System.Drawing.Point(95, 36);
+            this.textBox_DirectoryName.Name = "textBox_DirectoryName";
+            this.textBox_DirectoryName.Size = new System.Drawing.Size(211, 19);
+            this.textBox_DirectoryName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "フォルダ名";
+            // 
+            // checkBox_IsOverwrite
+            // 
+            this.checkBox_IsOverwrite.AutoSize = true;
+            this.checkBox_IsOverwrite.Checked = true;
+            this.checkBox_IsOverwrite.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_IsOverwrite.Location = new System.Drawing.Point(64, 84);
+            this.checkBox_IsOverwrite.Name = "checkBox_IsOverwrite";
+            this.checkBox_IsOverwrite.Size = new System.Drawing.Size(199, 16);
+            this.checkBox_IsOverwrite.TabIndex = 4;
+            this.checkBox_IsOverwrite.Text = "folder.jpgが存在する場合上書きする";
+            this.checkBox_IsOverwrite.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_IsHiddenFile
+            // 
+            this.checkBox_IsHiddenFile.AutoSize = true;
+            this.checkBox_IsHiddenFile.Checked = true;
+            this.checkBox_IsHiddenFile.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_IsHiddenFile.Location = new System.Drawing.Point(64, 129);
+            this.checkBox_IsHiddenFile.Name = "checkBox_IsHiddenFile";
+            this.checkBox_IsHiddenFile.Size = new System.Drawing.Size(107, 16);
+            this.checkBox_IsHiddenFile.TabIndex = 5;
+            this.checkBox_IsHiddenFile.Text = "隠しファイルにする";
+            this.checkBox_IsHiddenFile.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(39, 210);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(348, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // button_Execution
+            // 
+            this.button_Execution.Location = new System.Drawing.Point(167, 169);
+            this.button_Execution.Name = "button_Execution";
+            this.button_Execution.Size = new System.Drawing.Size(75, 23);
+            this.button_Execution.TabIndex = 7;
+            this.button_Execution.Text = "実行";
+            this.button_Execution.UseVisualStyleBackColor = true;
+            this.button_Execution.Click += new System.EventHandler(this.button_Execution_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.button_Execution);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.checkBox_IsHiddenFile);
+            this.Controls.Add(this.checkBox_IsOverwrite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_DirectoryName);
+            this.Controls.Add(this.checkBox_IsRecursion);
+            this.Controls.Add(this.button_DirectorySelect);
+            this.Name = "MainForm";
+            this.Text = "FolderThumbnailGenerator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_DirectorySelect;
+        private System.Windows.Forms.CheckBox checkBox_IsRecursion;
+        private System.Windows.Forms.TextBox textBox_DirectoryName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox_IsOverwrite;
+        private System.Windows.Forms.CheckBox checkBox_IsHiddenFile;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button_Execution;
+    }
+}
+
---
+++ FolderThumbnailGenerator/MainForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace FolderThumbnailGenerator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // folder.*としてコピーすることを許可する拡張子一覧
+        readonly string[] permissionImageExtList =
+        {
+            "jpg",
+            "jpeg",
+            "png",
+            "gif",
+            "bmp",
+        };
+
+        bool IsImageFile(string path)
+        {
+            string ext = Path.GetExtension(path);
+            if (string.IsNullOrWhiteSpace(ext)) return false; // 拡張子のないファイルは除外
+            ext = ext.Remove(0, 1); // 先頭のピリオドを削除
+
+            foreach (string testExt in permissionImageExtList)
+            {
+                if (ext == testExt) return true;
+            }
+
+            return false; // permissionImageExtListに存在しない拡張子だった
+        }
+
+        void AddAttributes(string path, FileAttributes attr)
+        {
+            FileAttributes fa = File.GetAttributes(path);
+            if ((fa & attr) == attr) return;
+            File.SetAttributes(path, fa | attr);
+        }
+
+        void RemoveAttributes(string path, FileAttributes attr)
+        {
+            var fa = File.GetAttributes(path);
+            if ((fa & attr) != attr) return;
+            File.SetAttributes(path, fa & ~attr);
+        }
+
+        void GenerateThumbnail(string path)
+        {
+            string sourceFile = Directory.GetFiles(path)?.FirstOrDefault(file => IsImageFile(file));
+
+            if (sourceFile != null)
+            {
+                //string thumbnailPath = $@"{path}\folder{Path.GetExtension(sourceFile)}";
+                string thumbnailPath = $@"{path}\folder.jpg";
+
+                RemoveAttributes(path, FileAttributes.ReadOnly); // ディレクトリの読み取り専用解除
+                if (File.Exists(thumbnailPath) && this.checkBox_IsOverwrite.Checked)
+                {
+                    // すでにファイルが存在する場合、読み取り専用解除
+                    RemoveAttributes(thumbnailPath, FileAttributes.ReadOnly);
+                }
+
+                try
+                {
+                    File.Copy(sourceFile, thumbnailPath, this.checkBox_IsOverwrite.Checked); // 上書き許可判定
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    MessageBox.Show(e.Message, "UnauthorizedAccessException", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                if (this.checkBox_IsHiddenFile.Checked) // 隠しファイル化判定
+                {
+                    AddAttributes(thumbnailPath, FileAttributes.Hidden);
+                }
+                else
+                {
+                    // CopyでUnauthorizedAccessExceptionが出る対策になるらしい
+                    // ファイルの属性がないのはよくないため？
+                    AddAttributes(thumbnailPath, FileAttributes.Normal);
+                }
+            }
+
+            // 再帰処理許可判定
+            if (!this.checkBox_IsRecursion.Checked) return;
+
+            foreach (string subDirPath in Directory.GetDirectories(path))
+            {
+                GenerateThumbnail(subDirPath);
+            }
+        }
+
+        private void button_DirectorySelect_Click(object sender, EventArgs e)
+        {
+            using (var cofd = new CommonOpenFileDialog())
+            {
+                if (this.textBox_DirectoryName.Text != null) cofd.DefaultFileName = this.textBox_DirectoryName.Text;
+                cofd.IsFolderPicker = true;
+                CommonFileDialogResult result = cofd.ShowDialog();
+                if (result == CommonFileDialogResult.Ok && !string.IsNullOrWhiteSpace(cofd.FileName))
+                {
+                    this.textBox_DirectoryName.Text = cofd.FileName;
+                }
+            }
+        }
+
+        private void button_Execution_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.textBox_DirectoryName.Text))
+            {
+                MessageBox.Show(@"作業フォルダを指定してください");
+                return;
+            }
+
+            GenerateThumbnail(this.textBox_DirectoryName.Text);
+            MessageBox.Show(@"終了しました");
+        }
+    }
+}
