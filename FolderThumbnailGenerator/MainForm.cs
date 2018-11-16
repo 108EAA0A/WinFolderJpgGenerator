e5f0b34a3288fdc5f38f58e61aec22031c8c37a2
108EAA0A <k_108e@outlook.jp>
2018-11-16 13:51:53 +0900

    app.configの生文字列の頭を改行

--- FolderThumbnailGenerator/MainForm.cs
+++ FolderThumbnailGenerator/MainForm.cs
@@ -37,7 +37,8 @@ namespace FolderThumbnailGenerator
             "cr2",
         };
 
-        readonly string defaultAppConfig = @"<?xml version=""1.0"" encoding=""utf-8""?>
+        readonly string defaultAppConfig =
+@"<?xml version=""1.0"" encoding=""utf-8""?>
 <configuration>
   <startup>
     <supportedRuntime version=""v4.0"" sku="".NETFramework,Version=v4.7.2"" />
