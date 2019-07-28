05bed1246355218b30fae9505557899626fa3f1e
108EAA0A <k_108e@outlook.jp>
2019-07-29 04:01:42 +0900

    内部プロジェクト名の変更

--- FolderThumbnailGenerator/Properties/Resources.Designer.cs
+++ FolderThumbnailGenerator/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FolderThumbnailGenerator.Properties {
+namespace WinFolderJpgGenerator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FolderThumbnailGenerator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FolderThumbnailGenerator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFolderJpgGenerator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
